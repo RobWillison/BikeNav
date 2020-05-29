c2aba6f7f529884cfe9c4bc858904fc095fa89bb
Rob Willison <Rob@willison.co.uk>
2020-05-29 20:47:32 +0100

    a

--- led_test.py
+++ led_test.py
@@ -1,20 +1,20 @@
 import board
 import neopixel
 from time import sleep
-pixels = neopixel.NeoPixel(board.D18, 30)
+pixels = neopixel.NeoPixel(board.D18, 12)
 pixels.brightness = 0.3
 
 def setPixel(i, r, g, b, brightness):
     i = i % 12
     brightness = 255 * brightness
-    pixels[i] = (r*brightness, g*brightness, b*brightness)
+    pixels[i] = (int(r*brightness), int(g*brightness), int(b*brightness))
 
 i = 0
 while True:
     setPixel(i-3, 1.0, 0.0, 0.0, 0.25)
     setPixel(i-2, 1.0, 0.0, 0.0, 0.5)
     setPixel(i-1, 1.0, 0.0, 0.0, 0.75)
-    setPixel(i-1, 1.0, 0.0, 0.0, 1.0)
+    setPixel(i, 1.0, 0.0, 0.0, 1.0)
     i += 1
-    sleep(0.2)
+    sleep(0.1)
     pixels.fill((0,0,0))
