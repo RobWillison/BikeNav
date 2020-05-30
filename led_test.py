eaed083ac97f6e8be54840d070b729d25db802da
Rob Willison <Rob@willison.co.uk>
2020-05-30 14:38:05 +0100

    a

--- led_test.py
+++ led_test.py
@@ -6,34 +6,38 @@ import busio
 import adafruit_lsm9ds0
 import math
 
-i2c = busio.I2C(board.SCL, board.SDA)
-sensor = adafruit_lsm9ds0.LSM9DS0_I2C(i2c)
-pixels = neopixel.NeoPixel(board.D18, 12)
-pixels.brightness = 0.3
+class BikeNav:
 
-angles = []
+    def __init__(self):
+        i2c = busio.I2C(board.SCL, board.SDA)
+        self.sensor = adafruit_lsm9ds0.LSM9DS0_I2C(i2c)
+        self.pixels = neopixel.NeoPixel(board.D18, 12)
+        pixels.brightness = 0.3
 
-def direction():
-    mag_x, mag_y, mag_z = sensor.magnetic
-    heading = 180 * math.atan2(mag_y,mag_x)/math.pi;
-    heading += 180;
+        self.angles = []
 
-    angles.append(heading)
-    angles = angles[-5:]
+    def direction(self):
+        mag_x, mag_y, mag_z = self.sensor.magnetic
+        heading = 180 * math.atan2(mag_y,mag_x)/math.pi;
+        heading += 180;
 
-def setLight():
-    angle = sum(angles) / len(angles)
-    pixel = round(angle / 33.0)
-    pixels.fill((0,0,0))
-    pixels[pixel] = (255,0,0)
+        self.angles.append(heading)
+        self.angles = angles[-5:]
 
-def setPixel(i, r, g, b, brightness):
-    i = i % 12
-    brightness = 255 * brightness
-    pixels[i] = (int(r*brightness), int(g*brightness), int(b*brightness))
+    def setLight(self):
+        angle = sum(angles) / len(angles)
+        pixel = round(angle / 33.0)
+        self.pixels.fill((0,0,0))
+        self.pixels[pixel] = (255,0,0)
+
+    def setPixel(self, i, r, g, b, brightness):
+        i = i % 12
+        brightness = 255 * brightness
+        self.pixels[i] = (int(r*brightness), int(g*brightness), int(b*brightness))
 
 
+nav = BikeNav()
 while True:
-    direction()
-    setLight()
+    nav.direction()
+    nav.setLight()
     sleep(0.1)
