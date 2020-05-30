ce31619a19ebf4326819b95b4d655d4a31ade596
Rob Willison <Rob@willison.co.uk>
2020-05-30 14:16:31 +0100

    a

--- led_test.py
+++ led_test.py
@@ -1,16 +1,23 @@
 import board
 import neopixel
 from time import sleep
-import time
 import board
 import busio
 import adafruit_lsm9ds0
+import math
 
 i2c = busio.I2C(board.SCL, board.SDA)
 sensor = adafruit_lsm9ds0.LSM9DS0_I2C(i2c)
 
-mag_x, mag_y, mag_z = sensor.magnetic
-print('Magnetometer (gauss): ({0:0.3f},{1:0.3f},{2:0.3f})'.format(mag_x, mag_y, mag_z))
+def direction():
+    mag_x, mag_y, mag_z = sensor.magnetic
+    heading = 180 * atan2(mag_y,mag_x)/M_PI;
+
+    if(heading < 0)
+      heading += 360;
+
+      return heading
+
 
 pixels = neopixel.NeoPixel(board.D18, 12)
 pixels.brightness = 0.3
@@ -20,12 +27,7 @@ def setPixel(i, r, g, b, brightness):
     brightness = 255 * brightness
     pixels[i] = (int(r*brightness), int(g*brightness), int(b*brightness))
 
-# i = 0
-# while True:
-#     setPixel(i-3, 1.0, 0.0, 0.0, 0.25)
-#     setPixel(i-2, 1.0, 0.0, 0.0, 0.5)
-#     setPixel(i-1, 1.0, 0.0, 0.0, 0.75)
-#     setPixel(i, 1.0, 0.0, 0.0, 1.0)
-#     i += 1
-#     sleep(0.1)
-#     pixels.fill((0,0,0))
+
+while True:
+    print(direction())
+    sleep(0.1)
