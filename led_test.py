a9e62930319f837a6607d9116c9c5c2cd174191a
Rob Willison <Rob@willison.co.uk>
2020-06-05 20:08:45 +0100

    a

--- led_test.py
+++ led_test.py
@@ -52,7 +52,7 @@ class BikeNav:
 
     def current_direction(self):
         mag_x, mag_y, mag_z = self.sensor.magnetic
-        heading = 180 * math.atan2(mag_z,mag_y)/math.pi;
+        heading = math.degrees(math.atan2(mag_z,mag_y));
         heading += 180;
         return heading
 
